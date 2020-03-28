00b0f70d9c6528bf9be30e0a0842100b3f4beb46
Carcharocles-megalodon <55915879+Carcharocles-megalodon@users.noreply.github.com>
2020-03-28 13:18:00 +0000

    added files and stuff

---
+++ DataParsing+OutputCOVID-19/DataParsing+OutputCOVID-19/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataParsing_Output_COVID_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
